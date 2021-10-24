970d774a28466ea285d49159be330921ca4d1372
ethayu <ethayu1@gmail.com>
2021-10-23 21:02:14 -0700

    Changes by ramgorthi

--- Main.java
+++ Main.java
@@ -6,11 +6,12 @@ class Main {
   static Scanner sc = new Scanner(System.in); // create scanner
   public static void main(String[] args) {
     Deck table = new Deck(); // deck players put cards into
-    Deck deck = new Deck(); // deck players draw from
+    Deck draw = new Deck(); // deck players draw from
 
     int players = 0; // declare players
-    System.out.println("How many players?"); // get user input for players
+    System.out.println("How many players? (between 1 and 7)"); // get user input for players
     players = sc.nextInt();
+
     Deck[] playerDecks = new Deck[players]; // create deck for each player
     for (int i = 0; i < playerDecks.length; i++) {
       playerDecks[i] = new Deck();
@@ -18,17 +19,45 @@ class Main {
     
 
     // deal 7 cards to each player at random
+    for (int i = 0; i <= players; i++) Deck.addCards(7, playerDecks[i], draw);
 
     System.out.println("-----UNO!-----");
 
+    // game start - while every player has at least 1 card, the game continues
     boolean uno = false;
-    while !(uno) {
+    while (!uno) {
+
+      // loop through every player, display cards
+      // allow user to choose which card to play
+      // if card is incompatible with last card in table deck, make them play again
+      // allow user option to draw card
       for (int i = 0; i <= players; i++) {
+
+        // player UI
         System.out.println("Player " + (i + 1) + " - which card will you play?");
-        System.out.println()
-        if (playerDecks[i].size()==0) uno = true;
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
+        System.out.println();
+        // display cards
+        System.out.println("Would you like to play or draw a card?");
+        System.out.println("Type '1' for play and '0' for draw");
+        if (playerDecks[i].deck.size() == 1) {
+          //uno print statement
+          if (!Card.isCompatible(playerDecks[i].deck.get(0), table.deck.get(table.deck.size() - 1)))
+            Deck.addCards(1, playerDecks[i], draw);
+
+          else {
+            System.out.println("Player " + i + 1 + " wins!");
+            break;
+          }
+        }
+        if (sc.nextInt() == 0) {
+          // take top card from draw deck and add card to player deck
+          Deck.addCards(1, playerDecks[i], draw);
+          continue;
+        }
+        else {
+          if (makeMove(playerDecks, table, i) == -1) makeMove(playerDecks, table, i);
+        }
+        if (playerDecks[i].deck.size() ==0) uno = true;
        }
     }
     
