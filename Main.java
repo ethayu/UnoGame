85f3f336502099501d45a75d60eff750319aa9ba
Ram Gorthi <30302781+ramgorthi04@users.noreply.github.com>
2021-10-24 18:48:26 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,9 +9,18 @@ class Main {
     draw.makeFullDeck();// deck players draw from
 
     int players = 0; // declare players
-    System.out.println("How many players? (between 2 and 7)"); // get user input for players
-    players = sc.nextInt();
-
+    
+    boolean correct = false; // tracks if number of players user entered is within bounds
+    while (!correct) {
+      System.out.println("How many players? (between 2 and 7)"); // get user input for players
+      players = sc.nextInt();
+      
+      if (players >= 2 && players <= 7)
+          correct = true;
+      else if (players > 7) System.out.println("That is too many players.");
+      else if (players < 2) System.out.println("That is not enough players.");
+    }
+      
     Deck[] playerDecks = new Deck[players]; // create deck for each player
     for (int i = 0; i < playerDecks.length; i++) {
       playerDecks[i] = new Deck();
@@ -29,36 +38,48 @@ class Main {
     // if card is incompatible with last card in table deck, make them play again
     // allow user option to draw card
     while (true) for (int i = 0; i < players; i += d) {
-
+      
+      // clear past players cards
+      clearScreen();
+      
       // player UI
       System.out.println("Table card: |" + table.getTop() + "|");
       System.out.println("Player " + (i + 1) + " - which card will you play?");
       Deck.showUser(playerDecks[i]);
-      System.out.println("Would you like to play or draw a card?");
-      System.out.println("Type '1' for play and '0' for draw");
-      if (playerDecks[i].deck.size() == 1) {
-        System.out.println("-----UNO!-----");
-        if (!Card.isCompatible(playerDecks[i].deck.get(0), table.getTop()))
+      
+      correct = false; // correct tracks whether or not the user chooses a valid move (draw or play)
+      while (!correct) {
+        
+        System.out.println("Would you like to play or draw a card?");
+        System.out.println("Type '1' for play and '0' for draw");
+        if (playerDecks[i].deck.size() == 1) {
+          System.out.println("-----UNO!-----");
+          if (!Card.isCompatible(playerDecks[i].deck.get(0), table.getTop()))
+            Deck.addCards(1, playerDecks[i], draw);
+
+          else {
+            System.out.println("Player " + i + 1 + " wins!");
+            break;
+          }
+        } else if (sc.nextInt() != 0 && sc.nextInt != 1) {
+            System.out.println("You must either draw or play a card. Try again.");
+
+        } else if (sc.nextInt() == 0) {
+          // take top card from draw deck and add card to player deck
           Deck.addCards(1, playerDecks[i], draw);
-
-        else {
-          System.out.println("Player " + i + 1 + " wins!");
-          break;
+          correct = true;
+        } else {
+          if (makeMove(playerDecks, table, i)) makeMove(playerDecks, table, i);
+          if (table.getTop().getClass().getName().equals("PlusTwo")) Deck.addCards(2, playerDecks[i + d], draw);
+          else if (table.getTop().getClass().getName().equals("Reverse")) d *= -1;
+          else if (table.getTop().getClass().getName().equals("Skip")) i += d;
+          else if (table.getTop().getClass().getName().equals("WildCard")) if (makeWildCardMove(playerDecks, table, i)) makeWildCardMove(playerDecks, table, i);
+          else if (table.getTop().getClass().getName().equals("WildCardPlusFour")){
+            if (makeWildCardMove(playerDecks, table, i)) makeWildCardMove(playerDecks, table, i);
+            Deck.addCards(4, playerDecks[i + d], draw);
+          }
+          correct = true;
         }
-      } else if (sc.nextInt() == 0) {
-        // take top card from draw deck and add card to player deck
-        Deck.addCards(1, playerDecks[i], draw);
-      } else {
-        if (makeMove(playerDecks, table, i)) makeMove(playerDecks, table, i);
-        if (table.getTop().getClass().getName().equals("PlusTwo")) Deck.addCards(2, playerDecks[i + d], draw);
-        else if (table.getTop().getClass().getName().equals("Reverse")) d *= -1;
-        else if (table.getTop().getClass().getName().equals("Skip")) i += d;
-        else if (table.getTop().getClass().getName().equals("WildCard")) if (makeWildCardMove(playerDecks, table, i)) makeWildCardMove(playerDecks, table, i);
-        else if (table.getTop().getClass().getName().equals("WildCardPlusFour")){
-          if (makeWildCardMove(playerDecks, table, i)) makeWildCardMove(playerDecks, table, i);
-          Deck.addCards(4, playerDecks[i + d], draw);
-        }
-
       }
     }
   }
@@ -85,5 +106,10 @@ class Main {
     if (cardIndex > playerDecks.length || cardIndex < playerDecks.length) return true;
     Deck.moveCard(cardIndex-1, playerDecks[i], table);
     return false;
+  }
+  
+  public static void clearScreen() {  
+    System.out.print("\033[H\033[2J");  
+    System.out.flush();  
   }
 }
